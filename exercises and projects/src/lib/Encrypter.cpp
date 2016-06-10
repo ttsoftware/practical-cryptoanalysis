f0ac8d1275ffd612f0dcd0e62fb70a7514946a3a
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-06-10 13:27:21 +0200

    class cleanup

--- exercises and projects/src/lib/Encrypter.cpp
+++ exercises and projects/src/lib/Encrypter.cpp
@@ -1,15 +1,6 @@
 #include "Encrypter.h"
 
 template<size_t T>
-mutex Encrypter<T>::coutMutex;
-
-template<size_t T>
-bitset<T> Encrypter<T>::encrypt(bitset<T> key,
-                                bitset<T> plaintext) {
-    return key ^ plaintext;
-}
-
-template<size_t T>
 void Encrypter<T>::chain(bitset<T> input,
                          bitset<T> challenge,
                          unordered_map<bitset<T>, bitset<T>> *rainbowTable) {
@@ -112,43 +103,6 @@ void Encrypter<T>::breakKey(unordered_map<bitset<T>, bitset<T>> *map,
     cout << result << endl;
     cout << "Actual:" << endl;
     cout << secret << endl;
-}
-
-template<size_t T>
-bitset<T> Encrypter<T>::bruteforce(bitset<T> plainText,
-                                   bitset<T> cipherText,
-                                   time_t maxSeconds,
-                                   uint64_t keySpaceStart,
-                                   uint64_t keySpaceEnd) {
-
-    thread::id tid = this_thread::get_id();
-
-    cout << "Thread: " << tid << " : " << keySpaceStart << " - " << keySpaceEnd << endl;
-
-    bitset<T> incKey(keySpaceStart);
-    bitset<T> tempCipher;
-
-    uint64_t cycleCount = 0;
-
-    // stop if we reach the end of keyspace
-    while (time(NULL) <= maxSeconds && incKey.count() < keySpaceEnd) {
-        tempCipher = Encrypter<T>::encrypt(incKey, plainText);
-
-        if (tempCipher == cipherText) {
-            cout << incKey << endl;
-            break;
-        }
-
-        incKey = Encrypter<T>::increment(incKey);
-        cycleCount++;
-    }
-
-    coutMutex.lock();
-    cout << "Thread: " << tid << " : " << cycleCount << " cycles" << endl;
-    cout << "Thread: " << tid << " : " << incKey << " key" << endl;
-    coutMutex.unlock();
-
-    return incKey;
 }
 
 template<size_t T>
--- exercises and projects/src/lib/Encrypter.h
+++ exercises and projects/src/lib/Encrypter.h
@@ -16,21 +16,7 @@ using namespace std;
 template<size_t T> class Encrypter {
 
 public:
-    static mutex coutMutex;
-
-    static bitset<T> bruteforce(bitset<T> plainText,
-                                bitset<T> cipherText,
-                                time_t maxSeconds,
-                                uint64_t keySpaceStart,
-                                uint64_t keySpaceEnd);
-
-    static bitset<T> encrypt(bitset<T> key, bitset<T> plaintext);
-
     static bitset<28> md5Redux(bitset<T> input);
-
-    static bitset<28> reduceSize(bitset<T> input);
-
-    static bitset<56> concat(bitset<28> inputA, bitset<28> inputB);
 
     static bitset<T> rainbowLookup(bitset<T> cipher, int rainbowFunction, int chainLength);
 
@@ -48,6 +34,10 @@ public:
 
 private:
     static bitset<T> increment(bitset<T> input);
+
+    static bitset<56> concat(bitset<28> inputA, bitset<28> inputB);
+
+    static bitset<28> reduceSize(bitset<T> input);
 
     static bitset<T> chainLookup(bitset<T> start, int rainbowFunction, int chainLength);
 };
