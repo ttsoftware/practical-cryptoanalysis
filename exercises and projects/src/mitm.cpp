47ad81983a6e4ce1a0bfd58334b8fe33f8de4fb8
Troels Thomsen <tt.ttsoftware@gmail.com>
2016-06-17 15:51:38 +0200

    Test included

--- exercises and projects/src/mitm.cpp
+++ exercises and projects/src/mitm.cpp
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+void test(unsigned char plaintext[2][2]) {
+
+    unsigned char keys[4];
+    keys[0] = 0x00;
+    keys[1] = 0x01;
+    keys[2] = 0x02;
+    keys[3] = 0x03;
+
+    unsigned char plaintextResult[2];
+    unsigned char cipherResult[2];
+
+    Encrypter<0>::feistel(plaintext[0], cipherResult, keys, 4);
+    Encrypter<0>::inverseFeistel(cipherResult, plaintextResult, keys, 4);
+
+    bitset<8> b1 (cipherResult[0]);
+    bitset<8> b2 (cipherResult[1]);
+
+    bitset<8> p1 (plaintextResult[0]);
+    bitset<8> p2 (plaintextResult[1]);
+
+    bitset<8> p3 (plaintext[0][0]);
+    bitset<8> p4 (plaintext[0][1]);
+
+    cout << b1 << b2 << endl;
+    cout << p1 << p2 << endl;
+    cout << p3 << p4 << endl;
+
+    cout << (p1 == p3 && p2 == p4) << endl;
+
+    if (p1 == p3 && p2 == p4) {
+
+    }
+}
+
 int main() {
 
     unsigned char plaintext[2][2];
@@ -17,6 +51,8 @@ int main() {
     cipher[1][1] = 0xf6;
 
     unsigned char keys[4];
+
+    test(plaintext);
 
     Encrypter<0>::mitm(
             plaintext,
