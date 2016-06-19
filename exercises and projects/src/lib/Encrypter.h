bf39fa31559ec87a7929eb16158edf46bea145fa
Anders Rydbirk <ary@it-minds.dk>
2016-06-19 14:50:01 +0200

    DONE

--- exercises and projects/src/lib/Encrypter.h
+++ exercises and projects/src/lib/Encrypter.h
@@ -20,20 +20,42 @@ template<size_t T> class Encrypter {
 public:
 
     /**
-     *
+     * Test all they keys in the keyspace by encrypting forward and stores the result in the given map.
+     * @plaintext: The plaintext to encrypt.
+     * @map: The map to store the output and key-mapping in.
      */
     static void testKeysForward(unsigned char *plaintext, unordered_map<bitset<64>, vector<bitset<20>>> *map);
 
 
     /**
-     *
+     * Test all the keys in the key space by decrypting backwards.
+     * Checks the output in the given map and stores the matching pair.
+     * @cipher: The matching cipher for the given plaintext.
+     * @map: The map containing the output and key-pair.
+     * @pairs: The pairs of forward and backward keys.
      */
     static void testKeysBackward(unsigned char *cipher, unordered_map<bitset<64>, vector<bitset<20>>> *map, vector<tuple<bitset<20>,bitset<20>>> *pairs);
 
+    /**
+     * A wrapper function for the DES_ECB encryption. Converts the 20 bit key to a 7 byte char array.
+     * @input: The input to be encrypted / decrypted.
+     * @ouput: A output pointer.
+     * @key: The 20 bit key.
+     * @encrypt: true if encrypt, false if decrypt.
+     */
     static void encrypt(unsigned char *input, unsigned char *output, bitset<20> key, bool encrypt);
 
+    /**
+     * Converts the key to a 7 byte char array.
+     * @key: The 20 bit key.
+     * @output: The output array.
+     */
     static void convertKey(bitset<20> key, unsigned char * output);
 
+    /**
+     * Converts the given char array to a bitset.
+     * @input: Expected size = T.
+     */
     static bitset<T * 8> toBitset(unsigned char * input);
 
     /**
--- exercises and projects/src/main.cpp
+++ exercises and projects/src/main.cpp
@@ -15,26 +15,15 @@ int main() {
 
     //We need a char input for the key.
 
-    //This works
+    //This works. Generates the cipher by applying two layers.
     Encrypter<20>::encrypt((unsigned char *) &input, (unsigned char *) &temp, key1, true);
     Encrypter<20>::encrypt((unsigned char *) &temp, (unsigned char *) &cipher, key2, true);
 
-//    printf("Plain: [%s]\n", &input);
-
-//    for(int i = 0; i < 8; i++){
-//        cout << cipher[i] << endl;
-//    }
-
-//    unsigned char out[8];
-//    Encrypter<8>::convertKey(key, (unsigned char *)&out);
-//    bitset<56> res = Encrypter<7>::toBitset((unsigned char *)&out);
-
-//    cout << key << endl;
-//    cout << res << endl;
-
+    //Test all keys in the keyspace moving forward
     unordered_map<bitset<64>, vector<bitset<20>>> map;
     Encrypter<20>::testKeysForward((unsigned char *) &input, &map);
 
+    //Test all keys in the keyspace moving backward
     vector<tuple<bitset<20>,bitset<20>>> pairs;
     Encrypter<20>::testKeysBackward((unsigned char *) &cipher, &map, &pairs);
 
